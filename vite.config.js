7474c751c6c596bb20d61e2aa03093f749a11a7a
lilyhaschen <lilymedinafournier@gmail.com>
2025-06-25 15:26:15 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: 'https://github.com/lilyhaschen/lilypad/blob/main/public/index.html', // Specify the directory containing index.html
+  root: ./public/index.html', 
 });
